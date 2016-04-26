493de7b28e75e83463ffdb1aa6233e1a2a6bd309
RyanABailey <ryan.bailey@windowslive.com>
2016-04-26 13:27:12 +0000

    Update AssemblyInfo.cs

--- ScA/Properties/AssemblyInfo.cs
+++ ScA/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SAB")]
+[assembly: AssemblyTitle("ScA")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SAB")]
+[assembly: AssemblyProduct("ScA")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
